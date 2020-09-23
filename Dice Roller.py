14e16f95b185cf4487a73c8930aeca6c0251d2d9
Shaun Lockie <44776669+Shaunlockie@users.noreply.github.com>
2020-09-23 10:32:06 +0100

    Added a modifier to the dice roll

--- Dice Roller.py
+++ Dice Roller.py
@@ -23,50 +23,63 @@ buttonFrame.grid_columnconfigure(3, weight = 5)
 buttonFrame.grid_columnconfigure(4, weight = 5)
 buttonFrame.grid_columnconfigure(5, weight = 5)
 
-roll = StringVar()
-diceRolled = StringVar()
+modFrame = Frame(root)
+modFrame.grid(row=3, column=0, stick="nswe")
+modFrame.grid_columnconfigure(0, weight=1)
+modFrame.grid_columnconfigure(1, weight=1)
+
 img = Image.open("images\dice.png")
 img = img.resize((200,200),Image.ANTIALIAS)
 newImg = ImageTk.PhotoImage(img)
 
+var = StringVar(root)
+var.set("0")
+roll = IntVar()
+diceRolled = IntVar()
+modVar = IntVar()
+
 
 def main():
     rollLbl = Label(root, textvariable=roll, padx=10, pady = 10, justify=CENTER, background='white').grid(sticky="n",row=0, columnspan=5)
-    diceLabel = Label(root, image = newImg, textvariable = diceRolled, compound = "center", padx=10, pady = 10, justify=CENTER, background='white',font=("Courier", 39)).grid(sticky="nsew",row=1, columnspan=5)
-    d4Button = Button(buttonFrame, text="Roll D4", command=d4_roll, padx=10, pady = 10).grid(row=2, column=0, sticky="nsew")
-    d6Button = Button(buttonFrame, text="Roll D6", command=d6_roll, padx=10, pady = 10).grid(row=2, column=1, sticky="nsew")
-    d8Button = Button(buttonFrame, text="Roll D8", command=d8_roll, padx=10, pady = 10).grid(row=2, column=2, sticky="nsew")
-    d10Button = Button(buttonFrame, text="Roll D10", command=d10_roll, padx=10, pady = 10).grid(row=2, column=3, sticky="nsew")    
-    d20Button = Button(buttonFrame, text="Roll D20", command=d20_roll,  padx=10, pady = 10).grid(row=2, column=4, sticky="nsew")  
-    d100Button = Button(buttonFrame, text="Roll D100", command=d100_roll, padx=10, pady = 10).grid(row=2, column=5, sticky="nsew")  
+    diceLbl = Label(root, image = newImg, textvariable = diceRolled , compound = "center", padx=10, pady = 10, justify=CENTER, background='white',font=("Courier", 29)).grid(sticky="nsew",row=1, columnspan=5)
 
+    d4Button = Button(buttonFrame, text="Roll D4", command=lambda: d4_roll(modifier), padx=10, pady = 10, background='white').grid(row=2, column=0, sticky="nsew")
+    d6Button = Button(buttonFrame, text="Roll D6", command=lambda: d6_roll(modifier), padx=10, pady = 10, background='white').grid(row=2, column=1, sticky="nsew")
+    d8Button = Button(buttonFrame, text="Roll D8", command=lambda: d8_roll(modifier), padx=10, pady = 10, background='white').grid(row=2, column=2, sticky="nsew")
+    d10Button = Button(buttonFrame, text="Roll D10", command=lambda: d10_roll(modifier), padx=10, pady = 10, background='white').grid(row=2, column=3, sticky="nsew")    
+    d20Button = Button(buttonFrame, text="Roll D20", command=lambda: d20_roll(modifier),  padx=10, pady = 10, background='white').grid(row=2, column=4, sticky="nsew")  
+    d100Button = Button(buttonFrame, text="Roll D100", command=lambda: d100_roll(modifier), padx=10, pady = 10, background='white').grid(row=2, column=5, sticky="nsew")
     
-def d4_roll():
-    d4 = random.randint(1,4)
+    modLbl = Label(modFrame, text = "Modifier:", width = 20, justify=LEFT, background='white').grid(row = 3, columnspan = 4,  sticky="nsew")
+    modifier = Spinbox(modFrame, from_=-5, to=10, textvariable=modVar)
+    modifier.grid(row=3, column=5, sticky="nsew")
+            
+def d4_roll(modifier):
+    d4 = random.randint(1,4) + int(modifier.get())
     roll.set("D4 result was  " + str(d4))
     diceRolled.set(str(d4))
     return d4 
 
-def d6_roll():
-    d6 = random.randint(1,6)
+def d6_roll(modifier):
+    d6 = random.randint(1,6) + int(modifier.get())
     roll.set("D6 result was " + str(d6))
     diceRolled.set(str(d6))
     return d6
 
-def d8_roll():
-    d8 = random.randint(1,8)
+def d8_roll(modifier):
+    d8 = random.randint(1,8) + int(modifier.get())
     roll.set("D8 result was " + str(d8))
     diceRolled.set(str(d8))
     return d8
 
-def d10_roll():
-    d10 = random.randint(1,8)
+def d10_roll(modifier):
+    d10 = random.randint(1,8) + int(modifier.get())
     roll.set("D10 Result was " + str(d10))
     diceRolled.set(str(d10))
     return d10
     
-def d20_roll():
-    d20 = random.randint(1,20)
+def d20_roll(modifier):
+    d20 = random.randint(1,20) + int(modifier.get())
     if d20 == 1:
         roll.set("D20 result was " + str(d20) + "   Critical Fail!")
         diceRolled.set(str(d20))
@@ -78,11 +91,12 @@ def d20_roll():
         diceRolled.set(str(d20))
     return d20
     
-def d100_roll():
-    d100 = random.randint(1,100)
+def d100_roll(modifier):
+    d100 = random.randint(1,100) + int(modifier.get())
+    d100Moded = d100 
     roll.set("D100 result was " + str(d100))
     diceRolled.set(str(d100))
-    return d100
+    return d100 
 
 
 main()
